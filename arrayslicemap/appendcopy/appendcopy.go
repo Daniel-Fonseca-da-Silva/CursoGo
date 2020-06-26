d45aa112bc4c056f6be49d875c7ba73083d0a0f8
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2020-06-25 21:21:00 -0300

    Criada a pasta appendcopy e adicionado arquivo de mesmo nome

---
+++ arrayslicemap/appendcopy/appendcopy.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	array1 := [3]int{1, 2, 3}
+	var slice1 []int
+
+	// array = append(array1, 4, 5, 6)
+	slice1 = append(slice1, 4, 5, 6)
+	fmt.Println(array1, slice1)
+
+	slice2 := make([]int, 2)
+	// copy alvo, fonte
+	copy(slice2, slice1)
+	fmt.Println(slice2)
+}
