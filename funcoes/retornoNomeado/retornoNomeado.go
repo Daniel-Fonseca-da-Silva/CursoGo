469239fbdfae26a1d31e78cf7a601d5c52f61a48
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-04-09 22:58:16 -0300

    Criado o arquivo retornoNomeado na pasta funcoes

---
+++ funcoes/retornoNomeado/retornoNomeado.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func trocar(p1, p2 int) (segundo, primeiro int) {
+	segundo = p2
+	primeiro = p1
+
+	return // Retorno limpo
+}
+
+func main() {
+	x, y := trocar(2, 3)
+	fmt.Println(x, y)
+
+	segundo, primeiro := trocar(7, 1)
+	fmt.Println(segundo, primeiro)
+}
